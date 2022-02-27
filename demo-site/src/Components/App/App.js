15f1ae3195b03146129225097ca5025c656b428e
Tamar Miller <rmiller25@student.touro.edu>
2022-02-27 17:39:40 -0500

    header with errors

--- demo-site/src/Components/App/App.js
+++ demo-site/src/Components/App/App.js
@@ -1,16 +1,22 @@
-import {React} from 'react';
-import {Home} from '../home/home';
-import TodoList from '../todo/TodoList';
 
-function App(){
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "../home/home";
+// import Todo/ist from '../todo/TodoList';
+import { Header } from "../header/header";
+
+function App() {
   return (
-    
     <div>
-   
- <TodoList/>
-    
-  
-   </div>
-  )
+      
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/todo" element={<TodoList />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 export default App;
